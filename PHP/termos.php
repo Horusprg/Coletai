f54d384004a0fcb62ef60d1c4c4b75cd11fc5594
Horusprg <newconnect7@hotmail.com>
2021-05-02 20:50:13 -0300

    Desenvolvimento 02/05/2021

---
+++ PHP/termos.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet" />
+		<link href="C:\Users\Hayla\AppData\Local\atom\app-1.55.0\termos.css" rel="stylesheet" />
+		<title>Termos de uso</title>
+		<link rel = "stylesheet" type="text/css" href="css/hover.css"/>
+		<link rel = "stylesheet" type="text/css" href="css/termos.css"/>
+	</head>
+	<body>
+		<nav>
+			<a href="Servico.html"><button class="servico">SERVIÇO</button></a>
+			<a href="About.html"><button class="sobre">SOBRE</button></a>
+			<a href="Index.html"><button class="home">HOME</button></a>
+			<div class="logo"></div>
+			<a href="Login.html"><button class="entrar">ENTRAR</button></a>
+		</nav>
+		<div class="rodape">
+			<a href="politica.html"><button class="rodape_txt">Políticas de privacidade</button></a>
+			<a href="termos.html"><button class="rodape_txt_1">Termos de uso &nbsp;|</button></a>
+		</div>
+    <div class="divisoria"></div>
+    <div class="container">
+      <h1>TERMOS DE USO</h1>
+      <h2>1. Do objeto</h2>
+      <p> A plataforma visa licenciar o uso de seu software, website, aplicativos e demais ativos de propriedade intelectual, fornecendo ferramentas para auxiliar e dinamizar o dia a dia dos seus usuários. <br> <br> A plataforma caracteriza-se pela prestação do seguinte serviço: coleta de informações relacionadas a empresas de entulho. </p>
+      <h2>2. Da aceitação</h2>
+      <p> O presente Termo estabelece obrigações contratadas de livre e espontânea vontade, por tempo indeterminado, entre a plataforma e as pessoas físicas ou jurídicas, usuárias do site. <br> <br> Ao utilizar a plataforma o usuário aceita integralmente as presentes normas e compromete-se a observá-las, sob o risco de aplicação das penalidades cabíveis. <br> <br> A aceitação do presente instrumento é imprescindível para o acesso e para a utilização de quaisquer serviços fornecidos pela empresa. Caso não concorde com as disposições deste instrumento, o usuário não deve utilizá-los.</p>
+      <h2>3. Do acesso dos usuários</h2>
+      <p> Serão utilizadas todas as soluções técnicas à disposição do responsável pela plataforma para permitir o acesso ao serviço 24 (vinte e quatro) horas por dia, 7 (sete) dias por semana. No entanto, a navegação na plataforma ou em alguma de suas páginas poderá ser interrompida, limitada ou suspensa para atualizações, modificações ou qualquer ação necessária ao seu bom funcionamento. </p>
+      <h2>4. Do cadastro</h2>
+      <p> Ao se cadastrar o usuário deverá informar dados completos, recentes e válidos, sendo de sua exclusiva responsabilidade manter referidos dados atualizados, bem como o usuário se compromete com a veracidade dos dados fornecidos. <br> <br> O usuário se compromete a não informar seus dados cadastrais e/ou de acesso à plataforma a terceiros, responsabilizando-se integralmente pelo uso que deles seja feito. <br> <br> Menores de 18 anos e aqueles que não possuírem plena capacidade civil deverão obter previamente o consentimento expresso de seus responsáveis legais para utilização da plataforma e dos serviços ou produtos, sendo de responsabilidade exclusiva dos mesmos o eventual acesso por menores de idade e por aqueles que não possuem plena capacidade civil sem a prévia autorização.
+<br> <br> Mediante a realização do cadastro o usuário declara e garante expressamente ser plenamente capaz, podendo exercer e usufruir livremente dos serviços e produtos.
+<br> <br> O usuário deverá fornecer um endereço de e-mail válido, através do qual o site realizará todas comunicações necessárias.
+<br> <br> Após a confirmação do cadastro, o usuário possuirá um login e uma senha pessoal, a qual assegura ao usuário o acesso individual à mesma. Desta forma, compete ao usuário exclusivamente a manutenção de referida senha de maneira confidencial e segura, evitando o acesso indevido às informações pessoais.
+<br> <br> Toda e qualquer atividade realizada com o uso da senha será de responsabilidade do usuário, que deverá informar prontamente a plataforma em caso de uso indevido da respectiva senha.
+<br> <br> O usuário, ao aceitar os Termos e Política de Privacidade, autoriza expressamente a plataforma a coletar, usar, armazenar, tratar, ceder ou utilizar as informações derivadas do uso dos serviços, do site e quaisquer plataformas, incluindo todas as informações preenchidas pelo usuário no momento em que realizar ou atualizar seu cadastro, além de outras expressamente descritas na <a href="politica.html">Política de Privacidade</a> que deverá ser autorizada pelo usuário. </p>
+      <h2>5. Das responsabilidades</h2>
+      <p> É de responsabilidade do usuário:
+<br> <br> a) defeitos ou vícios técnicos originados no próprio sistema do usuário;
+<br> <br> b) a correta utilização da plataforma, dos serviços ou produtos oferecidos, prezando pela boa convivência, pelo respeito e cordialidade entre os usuários;
+<br> <br> c) pelo cumprimento e respeito ao conjunto de regras disposto nesse Termo de Condições Geral de Uso, na respectiva Política de Privacidade e na legislação nacional e internacional;
+<br> <br> d) pela proteção aos dados de acesso à sua conta/perfil (login e senha).
+<br> <br> É de responsabilidade da plataforma Coletaí:
+<br> <br> a) as informações que foram por ele divulgadas.
+<br> <br> b) os conteúdos ou atividades ilícitas praticadas através da sua plataforma.
+<br> <br> A plataforma não se responsabiliza por links externos contidos em seu sistema que possam redirecionar o usuário à ambiente externo a sua rede.
+<br> <br> Não poderão ser incluídos links externos ou páginas que sirvam para fins comerciais ou publicitários ou quaisquer informações ilícitas, violentas, polêmicas, pornográficas, xenofóbicas, discriminatórias ou ofensivas. </p>
+      <h2>6. Dos direitos autorais</h2>
+      <p> O presente Termo de Uso concede aos usuários uma licença não exclusiva, não transferível e não sublicenciável, para acessar e fazer uso da plataforma e dos serviços por ela disponibilizados.
+<br> <br> A estrutura do site, as marcas, logotipos, nomes comerciais, layouts, gráficos e design de interface, imagens, ilustrações, fotografias, apresentações, vídeos, conteúdos escritos e de som e áudio, programas de computador, banco de dados, arquivos de transmissão e quaisquer outras informações e direitos de propriedade intelectual da razão social Coletaí, observados os termos da Lei da Propriedade Industrial (Lei nº 9.279/96), Lei de Direitos Autorais (Lei nº 9.610/98) e Lei do Software (Lei nº 9.609/98), estão devidamente reservados.
+<br> <br> Este Termos de Uso não cede ou transfere ao usuário qualquer direito, de modo que o acesso não gera qualquer direito de propriedade intelectual ao usuário, exceto pela licença limitada ora concedida.
+<br> <br> O uso da plataforma pelo usuário é pessoal, individual e intransferível, sendo vedado qualquer uso não autorizado, comercial ou não-comercial. Tais usos consistirão em violação dos direitos de propriedade intelectual da razão social Coletaí, puníveis nos termos da legislação aplicável.
+<br> <br> Não será permitido ceder, vender, alugar ou transferir, de qualquer forma, a conta, que é pessoal e intransferível.
+<br> <br> Caberá ao usuário assegurar que o seu equipamento seja compatível com as características técnicas que viabilize a utilização da plataforma e dos serviços ou produtos. </p>
+      <h2>7. Das sanções</h2>
+      <p> Sem prejuízo das demais medidas legais cabíveis, a razão social Coletaí poderá, a qualquer momento, advertir, suspender ou cancelar a conta do usuário:
+<br> <br> a) que violar qualquer dispositivo do presente Termo;
+<br> <br> b) que descumprir os seus deveres de usuário;
+<br> <br> c) que tiver qualquer comportamento fraudulento, doloso ou que ofenda a terceiros. </p>
+      <h2>8. Da rescisão</h2>
+      <p> A não observância das obrigações pactuadas neste Termo de Uso ou da legislação aplicável poderá, sem prévio aviso, ensejar a imediata rescisão unilateral por parte da razão social Coletaí e o bloqueio de todos os serviços prestados ao usuário. </p>
+      <h2>9. Das alterações</h2>
+      <p> Os itens descritos no presente instrumento poderão sofrer alterações, unilateralmente e a qualquer tempo, por parte da plataforma, para adequar ou modificar os serviços, bem como para atender novas exigências legais. As alterações serão veiculadas pelo site Coletaí e o usuário poderá optar por aceitar o novo conteúdo ou por cancelar o uso dos serviços, caso seja assinante de algum serviço. </p>
+      <h2>10. Da política de privacidade</h2>
+      <p> Além do presente Termo, o usuário deverá consentir com as disposições contidas na respectiva <a href="politica.html">Política de Privacidade</a> a ser apresentada a todos os interessados dentro da interface da plataforma. </p>
+    </div>
+	</body>
+</html>
+	
